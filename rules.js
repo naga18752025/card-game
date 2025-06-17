685e5084d75fbc269152d0f87bba78acb606f795
naga18752025 <doshisha.ryota.813@gmail.com>
2025-06-17 13:34:25 +0900

    Add files via upload

--- rules.js
+++ rules.js
@@ -1,3 +1,15 @@
 function topback(){
     window.location.href = "index.html";
 }
+
+function nameExistCheck(){
+    Name = localStorage.getItem("username");
+    if(Name !== null){
+    document.getElementById("loginName").textContent = `${Name}としてログイン中`;
+    }else{
+    document.getElementById("loginName").textContent = `未ログイン`; 
+    document.getElementById("logout").style.display = "none";
+    }
+}
+
+nameExistCheck();
